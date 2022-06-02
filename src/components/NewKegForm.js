56f62bdf362174a9384f951cbd4316fe47cac006
chrisdfghjkl <cdepastene@gmail.com>
2022-06-02 16:51:45 -0700

    add placeholder form

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { v4 } from 'uuid';
 
 function NewKegForm(props){
   return (
     <React.Fragment>
-      <h3>placeholder</h3>
+      <div className="new-keg">
+        <h3>New Keg</h3>
+        <form onSubmit={handleNewKegFormSubmission}>
+          <div className="form-control">
+            <input 
+              type='text'
+              name='name'
+              placeholder="Name"
+              required
+            />
+            <input 
+              type='text'
+              name='type'
+              placeholder="Beer Type"
+              required
+            />
+            <input 
+              type='text'
+              name='brewery'
+              placeholder="Brewery"
+              required
+            />
+            <input 
+              type='number'
+              name='abv'
+              placeholder="%ABV"
+              required
+            />
+            <input 
+              type='number'
+              name='price'
+              placeholder="Price"
+              required
+            />
+            <Button type='submit' className='btn btn-success'>Add Keg</Button>
+          </div>
+      </form>
+      </div>
     </React.Fragment>
   );
 }
