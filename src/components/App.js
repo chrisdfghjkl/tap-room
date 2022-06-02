5455873082e3ad16ebf964a5306e87a07e362c18
chrisdfghjkl <cdepastene@gmail.com>
2022-06-02 15:09:30 -0700

    import stylesheet

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import KegList from "./KegList";
+import './../Styles.css';
 
 function App(){
   return ( 
     <React.Fragment>
       <Header />
+      <KegList />
     </React.Fragment>
   );
 }
