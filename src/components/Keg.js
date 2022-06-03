6143b02255cf0355c0172fd227c5d533b2c56ffc
chrisdfghjkl <cdepastene@gmail.com>
2022-06-02 22:12:12 -0700

    fix ABV format, add onclick div and new prop

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -5,11 +5,13 @@ function Keg(props){
   return (
     <React.Fragment>
       <div className="container">
-        <h3><b>{props.name}</b></h3>
-        <p><em>{props.type}</em></p>
-        <p>{props.brewery}</p>
-        <p>ABV: {props.abv}</p>
-        <p>${props.price}</p>
+        <div onClick = {() => props.whenKegClicked(props.id)}>
+          <h3><b>{props.name}</b></h3>
+          <p><em>{props.type}</em></p>
+          <p>{props.brewery}</p>
+          <p>ABV: {props.abv}%</p>
+          <p>${props.price}</p>
+        </div>
       </div>
     </React.Fragment>
   );
@@ -21,6 +23,8 @@ Keg.propTypes = {
   brewery: PropTypes.string,
   abv: PropTypes.string,
   price: PropTypes.string,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func
 };
 
 export default Keg;
