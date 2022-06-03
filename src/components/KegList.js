8ba560cc5b89340ee24c53f6e9e9a06b1b4f1bdc
chrisdfghjkl <cdepastene@gmail.com>
2022-06-02 17:02:37 -0700

    update list to work with form

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,36 +1,36 @@
 import React from "react";
 import Keg from "./Keg";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
-const defaultKegs = [
-  {
-    name: "Pale Ale",
-    type: "Pale Ale",
-    brewery: "Saranac",
-    abv: "5.5",
-    price: "$5.00"
-  },
-  {
-    name: "Burned Bridges",
-    type: "IPA",
-    brewery: "Ex Novo",
-    abv: "6.2",
-    price: "$6.00"
-  },
-  {
-    name: "Cold Chillin'",
-    type: "Cream Ale",
-    brewery: "Wild Ride",
-    abv: "4.8",
-    price: "$5.00"
-  }
-];
+// const defaultKegs = [
+//   {
+//     name: "Pale Ale",
+//     type: "Pale Ale",
+//     brewery: "Saranac",
+//     abv: "5.5",
+//     price: "$5.00"
+//   },
+//   {
+//     name: "Burned Bridges",
+//     type: "IPA",
+//     brewery: "Ex Novo",
+//     abv: "6.2",
+//     price: "$6.00"
+//   },
+//   {
+//     name: "Cold Chillin'",
+//     type: "Cream Ale",
+//     brewery: "Wild Ride",
+//     abv: "4.8",
+//     price: "$5.00"
+//   }
+// ];
 
-function KegList(){
+function KegList(props){
   return (
     <React.Fragment>
       <div id="keg-card-holder">
-        {defaultKegs.map((keg, index) =>
+        {props.kegList.map((keg, index) =>
           <div className="keg-card-single">
             <Keg name={keg.name}
               type={keg.type}
@@ -44,5 +44,9 @@ function KegList(){
     </React.Fragment>
   );
 }
+
+KegList.propTypes = {
+  kegList: PropTypes.array
+};
 
 export default KegList;
