fe14abc61d000700a92135a3930d99f29d94423b
chrisdfghjkl <cdepastene@gmail.com>
2022-06-02 21:56:27 -0700

    add detail method and import statement

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NewKegForm from './NewKegForm';
 import KegList from './KegList';
+import TicketDetail from './TicketDetail';
 
 class KegControl extends React.Component {
 
@@ -46,6 +47,11 @@ class KegControl extends React.Component {
     const newMainKegList = this.state.mainKegList.concat(newKeg);
     this.setState({mainKegList: newMainKegList,
                   formVisibleOnPage: false });
+  }
+
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.mainKegList.filter(keg => keg.id === id)[0];
+    this.setState({selectedKeg: selectedKeg});
   }
 
   render(){
