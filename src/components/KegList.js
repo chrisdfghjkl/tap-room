82bd96769964ce0f6ec71d1f9c40f81338841b29
chrisdfghjkl <cdepastene@gmail.com>
2022-06-02 17:30:53 -0700

    remove seed date

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -2,30 +2,6 @@ import React from "react";
 import Keg from "./Keg";
 import PropTypes from "prop-types";
 
-// const defaultKegs = [
-//   {
-//     name: "Pale Ale",
-//     type: "Pale Ale",
-//     brewery: "Saranac",
-//     abv: "5.5",
-//     price: "$5.00"
-//   },
-//   {
-//     name: "Burned Bridges",
-//     type: "IPA",
-//     brewery: "Ex Novo",
-//     abv: "6.2",
-//     price: "$6.00"
-//   },
-//   {
-//     name: "Cold Chillin'",
-//     type: "Cream Ale",
-//     brewery: "Wild Ride",
-//     abv: "4.8",
-//     price: "$5.00"
-//   }
-// ];
-
 function KegList(props){
   return (
     <React.Fragment>
