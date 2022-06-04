997f67b797561c3f634fa4322208f7b9f88940ef
chrisdfghjkl <cdepastene@gmail.com>
2022-06-03 20:46:29 -0700

    add fill status + styling

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -2,22 +2,38 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Keg(props){
-  let kegStatus = "100%";
-  
-  if (props.pints < 94) {
-    kegStatus = "75%";
-  } 
+  let kegStatus;
+  let statusClass = "full"
+  if (props.pints < 1) {
+    kegStatus = "Tapped!";
+    statusClass = "tapped";
+  } else if (props.pints < 13) {
+    kegStatus = ">10%";
+    statusClass = "ten";
+  } else if (props.pints < 32) {
+    kegStatus = "25%";
+    statusClass = "two-five";
+  } else if (props.pints < 65) {
+    kegStatus = "50%";
+    statusClass = "fifty";
+  } else if (props.pints < 94) {
+    kegStatus = "75%"
+    statusClass = "seven-five";
+  } else {
+    kegStatus = "100%"
+  }
 
   return (
     <React.Fragment>
       <div className="container">
         <div onClick = {() => props.whenKegClicked(props.id)}>
-          <h3><b>{props.name}</b></h3>
-          <p><em>{props.type}</em></p>
-          <p>{props.brewery}</p>
-          <p>ABV: {props.abv}%</p>
-          <p>${props.price}</p>
-          <p>Status: {kegStatus}</p>
+          <h3><b className="name">{props.name}</b> <span className="price">${props.price}</span></h3>
+          <hr />
+          <p className="brew-info"><em>{props.type}</em> - {props.brewery}</p>
+          <p className="brew-info">ABV: {props.abv}%</p>
+          <br/>
+          <hr />
+          <p className={statusClass}><b>Fill Status: {kegStatus}</b></p>
         </div>
       </div>
     </React.Fragment>
@@ -30,7 +46,7 @@ Keg.propTypes = {
   brewery: PropTypes.string,
   abv: PropTypes.string,
   price: PropTypes.string,
-  pints: PropTypes.string,
+  pints: PropTypes.number,
   id: PropTypes.string,
   whenKegClicked: PropTypes.func
 };
